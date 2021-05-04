7c85473dae9bbfe349908f9154df78dc38c19c16
宗翰 劉 <hankpathofprogramming@gmail.com>
2021-05-04 16:08:18 +0800

    Added Duck to Trex added Drop to Trex

--- Trex/Entities/Trex.cs
+++ Trex/Entities/Trex.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,6 +9,8 @@ namespace TrexRunner.Entities
 {
     class Trex : IGameEntity
     {
+        private const float RUN_ANIMATION_FRAME_LENGTH = 1 / 10f;
+
         private const float MIN_JUMP_HEIGHT = 40f;
 
         private const float GRAVITY = 1600f;
@@ -22,11 +25,20 @@ namespace TrexRunner.Entities
         public const int TREX_DEFAULT_SPRITE_POS_Y = 0;
         public const int TREX_DEFAULT_SPRITE_WIDTH = 44;
         public const int TREX_DEFAULT_SPRITE_HEIGHT = 52;
+
         private const float BLINK_ANIMATION_RANDOM_MIN = 2f;
         private const float BLINK_ANIMATION_RANDOM_MAX = 5f;
         private const float BLINK_ANIMATION_EYE_CLOSE_TIME = .5f;
 
+        private const int TREX_RUNNING_SPRITE_ONE_POS_X = TREX_DEFAULT_SPRITE_POS_X + TREX_DEFAULT_SPRITE_WIDTH * 2;
+        private const int TREX_RUNNING_SPRITE_ONE_POS_Y = 0;
 
+        private const int TREX_DUCKING_SPRITE_WIDTH = 59;
+
+        private const int TREX_DUCKING_SPRITE_ONE_POS_X = TREX_DEFAULT_SPRITE_POS_X + TREX_DEFAULT_SPRITE_WIDTH * 6;
+        private const int TREX_DUCKING_SPRITE_ONE_POS_Y = 0;
+
+        private const float DROP_VELOCITY = 500f;
 
         private Sprite _idleBackgroundSprite;
         private Sprite _idleSprite;
@@ -35,10 +47,16 @@ namespace TrexRunner.Entities
         private SoundEffect _jumpSound;
 
         private SpriteAnimation _blinkAnimation;
+        private SpriteAnimation _runAnimation;
+        private SpriteAnimation _duckAnimation;
+
         private Random _random;
 
         private float _verticalVelocity;
         private float _startPosY;
+        private float _dropVelocity;
+
+        
 
 
 
@@ -57,14 +75,28 @@ namespace TrexRunner.Entities
             _jumpSound = jumpSound;
 
             _random = new Random();
-            
+            _startPosY = Position.Y;
+
             _idleSprite = new Sprite(spriteSheet, TREX_DEFAULT_SPRITE_POS_X, TREX_DEFAULT_SPRITE_POS_Y, TREX_DEFAULT_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT);
             _idleBlinkSprite = new Sprite(spriteSheet, TREX_DEFAULT_SPRITE_POS_X + TREX_DEFAULT_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_POS_Y, TREX_DEFAULT_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT);
 
             _blinkAnimation = new SpriteAnimation();
             CreateBlinkAnimation();
+            //_blinkAnimation.Play();
 
-            _startPosY = Position.Y;
+            
+
+            _runAnimation = new SpriteAnimation();
+            _runAnimation.AddFrame(new Sprite(spriteSheet, TREX_RUNNING_SPRITE_ONE_POS_X, TREX_RUNNING_SPRITE_ONE_POS_Y, TREX_DEFAULT_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT), 0);
+            _runAnimation.AddFrame(new Sprite(spriteSheet, TREX_RUNNING_SPRITE_ONE_POS_X + TREX_DEFAULT_SPRITE_WIDTH, TREX_RUNNING_SPRITE_ONE_POS_Y, TREX_DEFAULT_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT), RUN_ANIMATION_FRAME_LENGTH);
+            _runAnimation.AddFrame(_runAnimation[0].Sprite, RUN_ANIMATION_FRAME_LENGTH * 2);
+            _runAnimation.Play();
+
+            _duckAnimation = new SpriteAnimation();
+            _duckAnimation.AddFrame(new Sprite(spriteSheet, TREX_DUCKING_SPRITE_ONE_POS_X, TREX_DUCKING_SPRITE_ONE_POS_Y, TREX_DUCKING_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT), 0);
+            _duckAnimation.AddFrame(new Sprite(spriteSheet, TREX_DUCKING_SPRITE_ONE_POS_X + TREX_DUCKING_SPRITE_WIDTH, TREX_DUCKING_SPRITE_ONE_POS_Y, TREX_DUCKING_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT), RUN_ANIMATION_FRAME_LENGTH);
+            _duckAnimation.AddFrame(_duckAnimation[0].Sprite, RUN_ANIMATION_FRAME_LENGTH * 2);
+            _duckAnimation.Play();
 
         }
 
@@ -78,6 +110,14 @@ namespace TrexRunner.Entities
             else if (State == TrexState.Jumping || State == TrexState.Falling)
             {
                 _idleSprite.Draw(spriteBatch, Position);
+            }
+            else if (State == TrexState.Running)
+            {
+                _runAnimation.Draw(spriteBatch, Position);
+            }
+            else if (State == TrexState.Ducking)
+            {
+                _duckAnimation.Draw(spriteBatch, Position);
             }
         }
 
@@ -94,19 +134,30 @@ namespace TrexRunner.Entities
             }
             else if (State == TrexState.Jumping || State == TrexState.Falling)
             {
+
                 if (_verticalVelocity >= 0)
                     State = TrexState.Falling;
 
-                Position = new Vector2(Position.X, Position.Y + _verticalVelocity * (float) gameTime.ElapsedGameTime.TotalSeconds);
+                Position = new Vector2(Position.X, Position.Y + (_verticalVelocity + _dropVelocity) * (float) gameTime.ElapsedGameTime.TotalSeconds);
                 _verticalVelocity += GRAVITY * (float) gameTime.ElapsedGameTime.TotalSeconds;
 
                 if(Position.Y >= _startPosY)
                 {
                     Position = new Vector2(Position.X, _startPosY);
                     _verticalVelocity = 0;
-                    State = TrexState.Idle;
+                    State = TrexState.Running;
                 }
             }
+            else if (State == TrexState.Running)
+            {
+                _runAnimation.Update(gameTime);
+            }
+            else if (State == TrexState.Ducking)
+            {
+                _duckAnimation.Update(gameTime);
+            }
+
+            _dropVelocity = 0;
         }
 
         private void CreateBlinkAnimation()
@@ -140,11 +191,41 @@ namespace TrexRunner.Entities
         {
             if (State != TrexState.Jumping || (_startPosY - Position.Y) < MIN_JUMP_HEIGHT)
                 return false;
-
-            State = TrexState.Falling;
+            
             _verticalVelocity = _verticalVelocity < CANCEL_JUMP_VELOCITY ? CANCEL_JUMP_VELOCITY: 0;
 
             return true;
+        }
+
+        public bool Duck()
+        {
+            if (State == TrexState.Jumping || State == TrexState.Falling)
+                return false;
+
+            State = TrexState.Ducking;
+            return true;
+        }
+
+        public bool Getup()
+        {
+            if (State != TrexState.Ducking)
+                return false;
+
+            State = TrexState.Running;
+
+            return true;
+        }
+
+        public bool Drop()
+        {
+            if (State != TrexState.Falling && State != TrexState.Jumping)
+                return false;
+
+            State = TrexState.Falling;
+
+            _dropVelocity = DROP_VELOCITY;
+            return true;
+
         }
     }
 }
--- Trex/System/InputController.cs
+++ Trex/System/InputController.cs
@@ -19,18 +19,32 @@ namespace TrexRunner.System
         {
             KeyboardState keyboardState = Keyboard.GetState();
 
-            if (keyboardState.IsKeyDown(Keys.Up) && !_previousKeyboardState.IsKeyDown(Keys.Up))
+            bool isJumpKeyPressed = keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.Space);
+            bool wasJumpKeyPressed = _previousKeyboardState.IsKeyDown(Keys.Up) || _previousKeyboardState.IsKeyDown(Keys.Space);
+
+            if (isJumpKeyPressed && !wasJumpKeyPressed)
             {
                 if (_trex.State != TrexState.Jumping)
                     _trex.BeginJump();
             }
             // if we check _previousKeyboardState.IsKeyDown
             // Cancel Jump will only be called on the frame the keyboard is released
-
-            else if (!keyboardState.IsKeyDown(Keys.Up))
+            else if (!isJumpKeyPressed && _trex.State == TrexState.Jumping)
             {
-                if (_trex.State == TrexState.Jumping)
-                    _trex.CancelJump();
+                _trex.CancelJump();
+            }
+
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                if(_trex.State == TrexState.Jumping || _trex.State == TrexState.Falling)
+                    _trex.Drop();
+                else
+                    _trex.Duck();
+            }
+
+            else if (_trex.State == TrexState.Ducking && !keyboardState.IsKeyDown(Keys.Down))
+            {
+                _trex.Getup();
             }
 
             // keyboardState is a struct, so it won't be referencing the same instance
